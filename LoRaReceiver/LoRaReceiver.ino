80a4661e76d91987307165fc59c7be797b51af7c
William Lucid <11734452+Tech500@users.noreply.github.com>
2023-10-20 13:00:07 -0400

    Update LoRaReceiver.ino

--- LoRaReceiver/LoRaReceiver.ino
+++ LoRaReceiver/LoRaReceiver.ino
@@ -1,3 +1,15 @@
+Based on Rui Santos project:
+
+/*********
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp32-lora-sensor-web-server/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*********/
 
 #include <LoRa.h>
 #include "boards.h"
